f5797b1522d93e6828cd84fba711b7ca64f5133c
Eric Poole <empoole114@gmail.com>
2024-05-29 14:38:12 -0400

    Add echo path

--- app/server.go
+++ app/server.go
@@ -25,13 +25,20 @@ func main() {
 
 	if(err != nil) {
 		fmt.Println("Error reading from connection buffer: ", err.Error())
+		os.Exit(1)
 	}
 
-	if(strings.HasPrefix(string(buffer), "GET / HTTP/1.1")) {
+	request_parts := strings.Split(string(buffer), "\r\n")
+	request_path := strings.Split(request_parts[0], " ")[1]
+
+	if(request_path == "/") {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if (request_path[0:6] == "/echo/") {
+		echo := request_path[6:]
+		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
+		conn.Write([]byte(response))		
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-
 
 }
