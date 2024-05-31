ebde25f0f26a94d58d1697c75642617fde24a174
Eric Poole <empoole114@gmail.com>
2024-05-31 12:24:28 -0400

    Refactors, get user agent

--- app/server.go
+++ app/server.go
@@ -7,6 +7,33 @@ import (
 	"strings"
 )
 
+
+type httpRequest struct {
+	method string
+	Headers map[string]string
+	path string
+	Body string
+}
+
+func (req *httpRequest) parseRequest(requestString string) *httpRequest {
+	requestParts := strings.Split(string(requestString), "\r\n")
+
+	req.method = strings.Split(requestParts[0], " ")[0]
+	req.path = strings.Split(requestParts[0], " ")[1]
+	req.Headers = make(map[string]string)
+
+	for i := 1; i < len(requestParts); i++ {
+		if requestParts[i] == "" {
+			req.Body = requestParts[i+1]
+			break
+		}
+		header := strings.Split(requestParts[i], ": ")
+		req.Headers[header[0]] = header[1]
+	}
+
+	return req
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -28,15 +55,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	request_parts := strings.Split(string(buffer), "\r\n")
-	request_path := strings.Split(request_parts[0], " ")[1]
+	request := new(httpRequest)
+	request.parseRequest(string(buffer))
 
-	if(request_path == "/") {
+	if(request.path == "/") {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if (request_path[0:6] == "/echo/") {
-		echo := request_path[6:]
+	} else if (request.path[0:6] == "/echo/") {
+		echo := request.path[6:]
 		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo), echo)
-		conn.Write([]byte(response))		
+		conn.Write([]byte(response))
+	} else if (request.path == "/user-agent") {
+		response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(request.Headers["User-Agent"]), request.Headers["User-Agent"])
+		conn.Write(([]byte(response)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
