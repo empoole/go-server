446553ecc79b8dc9a8ecbd47cb01b254b4d48236
Eric Poole <empoole114@gmail.com>
2024-06-04 21:55:13 -0400

    Clean up and add concurrency

--- app/server.go
+++ app/server.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 )
 
-
 type httpRequest struct {
 	method string
 	Headers map[string]string
@@ -15,40 +14,28 @@ type httpRequest struct {
 	Body string
 }
 
-func (req *httpRequest) parseRequest(requestString string) *httpRequest {
-	requestParts := strings.Split(string(requestString), "\r\n")
-
-	req.method = strings.Split(requestParts[0], " ")[0]
-	req.path = strings.Split(requestParts[0], " ")[1]
-	req.Headers = make(map[string]string)
-
-	for i := 1; i < len(requestParts); i++ {
-		if requestParts[i] == "" {
-			req.Body = requestParts[i+1]
-			break
-		}
-		header := strings.Split(requestParts[i], ": ")
-		req.Headers[header[0]] = header[1]
-	}
-
-	return req
-}
-
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer listener.Close()
 
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
 	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
+	_, err := conn.Read(buffer)
 
 	if(err != nil) {
 		fmt.Println("Error reading from connection buffer: ", err.Error())
@@ -70,5 +57,23 @@ func main() {
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+}
 
+func (req *httpRequest) parseRequest(requestString string) *httpRequest {
+	requestParts := strings.Split(string(requestString), "\r\n")
+
+	req.method = strings.Split(requestParts[0], " ")[0]
+	req.path = strings.Split(requestParts[0], " ")[1]
+	req.Headers = make(map[string]string)
+
+	for i := 1; i < len(requestParts); i++ {
+		if requestParts[i] == "" {
+			req.Body = requestParts[i+1]
+			break
+		}
+		header := strings.Split(requestParts[i], ": ")
+		req.Headers[header[0]] = header[1]
+	}
+
+	return req
 }
